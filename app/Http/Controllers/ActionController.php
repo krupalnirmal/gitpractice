9fd64c2283e7542f9697a083512c729e009acdad
Krupal Nirmal <krupalnirmal>
2023-08-26 12:49:36 +0530

    2nd commit

---
+++ app/Http/Controllers/ActionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ActionController extends Controller
+{
+    //
+
+   public function index(){
+    echo 'inside function';
+   }
+}
